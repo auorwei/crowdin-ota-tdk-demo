9b615334ed65af9c7eeb4d91a8ef1581aafc5caa
auorwei <30628027+auorwei@users.noreply.github.com>
2025-04-02 10:28:54 +0800

    Update crowdin-config.ts

--- app/crowdin-config.ts
+++ app/crowdin-config.ts
@@ -7,6 +7,3 @@ export const crowdinConfig = {
   languages: ['zh-CN', 'ru','fr','de','ja','ko'],
 }; 
 
-//014de74e2de5cd1ae2519ef0d56a82da63cff6e24d4f6ccb82494c00a88369aba2760c2b5a76ee0b
-
-//https://distributions.crowdin.net/e-cae066489dbabcfdb27c3b3jaw/manifest.json
